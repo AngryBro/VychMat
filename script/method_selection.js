5fef72ae3a1d59fd6385a3b29d71f88c791f398e
AngryBro <67253990+AngryBro@users.noreply.github.com>
2022-03-31 10:45:32 +1000

    .

--- script/method_selection.js
+++ script/method_selection.js
@@ -2,7 +2,7 @@ function select_method(select,button,random) {
 	document.getElementById(button).setAttribute('onclick',select.value);
 	var rnd_button = document.getElementById(random);
 	switch(select.value) {
-		case 'sqrt_method()': {
+		case 'sqrt_method(digits.value)': {
 			rnd_button.setAttribute('onclick',"random_symmetry_pd('matrix_A','vector_b',20)");
 			break;
 		}
