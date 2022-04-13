8ccef129b95ef9b19cb86d281e08578d243eb130
AngryBro <67253990+AngryBro@users.noreply.github.com>
2022-04-14 04:45:24 +1000

    .

--- script/edging_method.js
+++ script/edging_method.js
@@ -1,4 +1,4 @@
-function edging_method() {
+function edging_method(digits) {
 	var a = Matrix.input('matrix_A');
 	var b = Matrix.input('vector_b');
 	var output = document.getElementById('output');
@@ -7,6 +7,7 @@ function edging_method() {
 		console.log('Данные некорректны.');
 		return;
 	}
+	digits = digits<3?3:Math.round(digits);
 	var A = [];
 	var n = a.size.n;
 	var gamma = [];
@@ -29,9 +30,6 @@ function edging_method() {
 		}
 	}
 	A[n] = a.copy();
-	for(var i = 1; i<A.length; i++) {
-		A[i].log()
-	}
 	for(var k = 2; k<=n; k++) {
 		u[k] = A[k].vector(k);
 		u[k].pop(k);
@@ -43,22 +41,28 @@ function edging_method() {
 	D.length = n+1;
 	D[1] = new Matrix('vector',[1/A[1].get(1,1)]);
 	for(var i = 2; i<=n; i++) {
-		beta[i] = D[i-1].mult(u[i]);
-		gamma[i] = v[i].mult(D[i-1]);
+		beta[i] = D[i-1].mult(u[i]).mult(-1);
+		gamma[i] = v[i].mult(D[i-1]).mult(-1);
 		alpha[i] = 0;
-		for(var k = 1;k<n;k++) {
-			alpha[i] -= a.get(i,k)*beta[i].get(k);
+		for(var k = 1;k<i;k++) {
+			alpha[i] += a.get(i,k)*beta[i].get(k);
 		}
 		alpha[i] += a.get(i,i);
 		D[i] = new Matrix('0',i,i);
 		for(var j = 1; j<i; j++) {
 			D[i].set(j,i,beta[i].get(j)/alpha[i]);
-			D[i].set(i,j,gamma[i].get(j)/alpha[i]);
+			D[i].set(i,j,gamma[i].T().get(j)/alpha[i]);
 			for(var k = 1; k<i; k++) {
-				D[i].set(j,k,D[i-1].get(j,k)+beta[i].get(j)*gamma[i].get(k)/alpha[i]);
+				D[i].set(j,k,D[i-1].get(j,k)+beta[i].get(j)*gamma[i].T().get(k)/alpha[i]);
 			}
 		}
 		D[i].set(i,i,1/alpha[i]);
 	}
-	D[n].log();
+	var html = '\\(A='+a.tex(digits)+',~~~b='+b.tex(digits)+'\\)<br><br>'+
+	'\\(A^{-1}_{M.O} = '+D[n].tex(digits)+'\\)<br><br>'+
+	'\\(A^{-1} = '+a.invert().tex(digits)+'\\)<br><br>'+
+	'\\(x = '+D[n].tex(digits)+'\\cdot '+b.tex(digits)+' = '+
+	D[n].mult(b).tex(digits)+'\\)';
+	output.innerHTML = html;
+	MathJax.typeset();
 }
