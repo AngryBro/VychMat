c4018986036cb071cf4e8eccb0f2af9bfe37ddc2
AngryBro <67253990+AngryBro@users.noreply.github.com>
2022-04-13 23:14:24 +1000

    .

--- script/QR_transform.js
+++ script/QR_transform.js
@@ -8,18 +8,4 @@ function QR_transform(digits) {
 		console.log('Данные некорректны.');
 		return;
 	}
-	var QR = A.QR();
-	var Q = QR.Q;
-	var R = QR.R;
-	var x = solve_triangle(R,Q.T().mult(b));
-	var x_star = A.invert().mult(b);
-	var output = document.getElementById('output');
-	var html = '\\(A='+A.tex(digits)+',~~~b='+b.tex(digits)+'\\)<br><br>'+
-	'\\(A = '+Q.tex(digits)+'\\cdot '+R.tex(digits)+'\\)<br><br>'+
-	'\\(Q^{-1} ='+Q.invert().tex(digits)+',~~~Q^T='+Q.T().tex(digits)+'\\)<br><br>'+
-	'\\('+R.tex(digits)+'\\cdot x = '+Q.T().mult(b).tex(digits)+'\\)<br><br>'+
-	'\\(x = '+x.tex(digits)+'\\)<br><br>'+
-	'Точное решение: \\(x^*=A^{-1}b='+x_star.tex(digits)+'\\)';
-	output.innerHTML = html;
-	MathJax.typeset();
 }
