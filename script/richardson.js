698285bd7814c3bb343bcc541ff31f01d95c03e4
AngryBro <67253990+AngryBro@users.noreply.github.com>
2022-05-05 11:30:28 +1000

    Create richardson.js

---
+++ script/richardson.js
@@ -0,0 +1,12 @@
+function richardson(digits) {
+	eval(input_iter);
+	//Определения
+	var x = [null];
+	var tau = [null];
+	var sigma = [null];
+	var eta = 1;
+	//Стартовые значения
+	//Динамика
+	//Ответ
+	eval(output_iter);
+}
